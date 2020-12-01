7af19fc334fef59c339874460d65a514ada4773c
MadelineHenry <madelinehenry04101996@gmail.com>
2020-12-01 14:22:17 +0100

    commit 2

---
+++ exercise2-1.js
@@ -0,0 +1,15 @@
+/*In the following sentence replace every occurence of the word clef/clé (including plurals) by the emoji 🔑 with a single pattern. */
+
+document.querySelector("body");
+const divText = document.createElement("div");
+document.body.appendChild(divText);
+
+const  text  =  `In french the word "key" used to be written "clef", but now it is written with an accent : "clé".
+You might find medieval stories, such as the "clefs sanglantes de barbe bleue" and more recents ones "les clés du mystère"`
+
+const pattern = /cl(é|ef)(s)?/gi;
+const newText = text.replace(pattern, '&#128273');
+
+divText.innerHTML = newText;
+
+console.log(newText);
---
+++ exercise2-2.js
@@ -0,0 +1,25 @@
+/*Write a regex pattern to validate that credit card number are well encrypted in the database. It can be encrypted with X or Y or Z and must be exactly 4 blocks of 4 chars.
+
+Example: XXYX XYYY XXXX YYYY is valid*/
+
+document.querySelector("body");
+
+const userInput = document.createElement("input");
+userInput.placeholder = "0000 0000 0000 0000";
+userInput.type = "text";
+document.body.appendChild(userInput);
+
+const textResult = document.createElement("div");
+document.body.appendChild(textResult);
+
+userInput.addEventListener("keyup", (e) => {
+    const inputValue = e.target.value;
+    const pattern = /^((X|Y|Z){4} ){3}(X|Y|Z){4}$/gi;
+
+    if(inputValue.match(pattern)){
+        textResult.innerHTML = "found";
+    }
+    else{
+        textResult.innerHTML = "Not found";
+    }
+})
